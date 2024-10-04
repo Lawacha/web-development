1e213e8c8e6b16ce3b85429ff815072acf294935
Supreme Suwal <supremesuwal9@gmail.com>
2024-10-04 22:50:34 +0545

    if-else Statement

--- day15.js
+++ day15.js
@@ -11,3 +11,63 @@ else if(size==="L")
     
 else{console.log("free");}
     
+let str=prompt("enter your string");
+if(str[0]==="a"&&str.length>3)
+    console.log("is a good string");
+else
+    console.log("is a bad string");
+
+// Practice question(day 15)
+let num=10;
+if(num%10==0){
+    console.log("good");
+}
+else
+{
+    console.log("bad");
+}
+
+// //question no two
+let username=prompt("enter your username");
+let age=prompt("enter your age");
+alert(`${username} is ${age} years old`);
+
+//question no three
+let quarter=Number(prompt("enter quarter 1-4"));
+switch(quarter){
+    case 1:
+        console.log("jan,febb,march");
+        break;
+        case 2:
+            console.log("april,may,june");
+            break;
+            case 3:
+                console.log("july, august, september");
+                break;
+                case 4:
+                    console.log(october,nov,dec);
+                    break;
+                    default:
+                        console.log("invalid");
+        
+}
+ 
+// //question four
+let a=prompt("enter a");
+let b=prompt("enter b");
+let c=prompt("enter c");
+if(a>b)
+{
+    if(a>c)
+        console.log("a is greatest");
+    else
+    console.log("c is greatest");
+}
+else{
+    if(b>c)
+        console.log("b is greatest")
+    else
+    console.log("c is greatest");
+}
+
+
