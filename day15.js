a51c460a769f40de94b1ae1a5c536b6782028981
Supreme Suwal <supremesuwal9@gmail.com>
2024-10-04 21:31:14 +0545

    if-else Statement

--- day15.js
+++ day15.js
@@ -1,12 +1,13 @@
-let size;
-prompt("enter size",size);
-if(size===XL);
-    console.log("price if rs 250");
-    else if(size===L)
-        console.log("price is rs 20");
-    else if(size ===m)
-        console.log("price is rs 100");
-    else if(size===s)
-        console.log("price is rs 50");
-    else
-    console.log("free");
+let size=prompt("enter your size");
+if(size==="XL")
+    {console.log("price if rs 250");}
+
+else if(size==="L")
+       { console.log("price is rs 20");}
+    else if(size ==="M")
+        {console.log("price is rs 100");}
+    else if(size==="S")
+    {console.log("price is rs 50");}
+    
+else{console.log("free");}
+    
