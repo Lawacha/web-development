acd6d7aeb75980d1007b7501b3669808f5a8f3b4
Supreme Suwal <supremesuwal9@gmail.com>
2024-10-05 09:41:05 +0545

    String Methods

---
+++ day16.js
@@ -0,0 +1,11 @@
+//question one
+let msg="help!";
+let newMsg=msg.trim().toUpperCase();
+
+//question two
+let name="ApnaCollege";
+name.slice(4,9);//Colle
+name.indexOf("na");//2
+let newName=name.replace("Apna","Our")//OurCollege
+ //question three
+ let newStr=name.slice(4).replace("l","t").replace("l","t");
